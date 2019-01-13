8119c0a2fdd52ba0c5f42c813c7a88b5e09da7e4
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2019-01-13 20:52:02 +0100

    console: Add device overview structure

--- pkg/webui/console/views/device-overview/index.js
+++ pkg/webui/console/views/device-overview/index.js
@@ -1,4 +1,4 @@
-// Copyright © 2018 The Things Network Foundation, The Things Industries B.V.
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,10 +16,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Col, Row, Container } from 'react-grid-system'
 
-import sharedMessages from '../../../lib/shared-messages'
-import Message from '../../../lib/components/message'
+import Tabs from '../../../components/tabs'
+import Icon from '../../../components/icon'
 
 import style from './device-overview.styl'
+
+const tabs = [
+  { title: 'Overview', name: 'overview' },
+  { title: 'Data', name: 'data' },
+  { title: 'Location', name: 'location' },
+  { title: 'Payload Formatter', name: 'develop' },
+  { title: 'General Settings', name: 'general-settings' },
+]
 
 @connect(function ({ device }, props) {
   return {
@@ -28,42 +36,52 @@ import style from './device-overview.styl'
 })
 class DeviceOverview extends React.Component {
 
+  handleTabChange () {
+
+  }
+
   get deviceInfo () {
     const {
-      device_id,
+      ids,
       description,
-      created_at,
-      updated_at,
     } = this.props.device
 
     return (
-      <div>
-        <h2 className={style.id}>
-          {device_id}
-        </h2>
-        <p>{description}</p>
-        <ul className={style.attributes}>
-          <li className={style.attributesEntry}>
-            <strong className={style.key}>
-              <Message content={sharedMessages.createdAt} />
-            </strong>
-            <span className={style.value}>{created_at.toLocaleDateString()}</span>
-          </li>
-          <li className={style.attributesEntry}>
-            <strong className={style.key}>
-              <Message content={sharedMessages.updatedAt} />
-            </strong>
-            <span className={style.value}>{updated_at.toLocaleDateString()}</span>
-          </li>
-        </ul>
+      <div className={style.overviewInfo}>
+        <div className={style.overviewInfoGeneral}>
+          <span className={style.devId}>{ids.device_id}</span>
+          <span className={style.devDesc}>{description}</span>
+          <div className={style.connectivity}>
+            <span className={style.activityDot} />
+            <span className={style.lastSeen}>Last seen 2 secs. ago</span>
+            <span className={style.frameCountUp}><Icon icon="arrow_upward" className={style.frameCountIcon} />89.139</span>
+            <span><Icon icon="arrow_downward" className={style.frameCountIcon} />0</span>
+          </div>
+        </div>
       </div>
     )
   }
 
   render () {
+    const { device_id } = this.props.device
     return (
       <Container>
         <Row className={style.head}>
+          <Col lg={12}>
+            <div className={style.title}>
+
+              <h2 className={style.id}>
+                {device_id}
+              </h2>
+            </div>
+            <Tabs
+              narrow
+              active="overview"
+              tabs={tabs}
+              onTabChange={this.handleTabChange}
+              className={style.tabs}
+            />
+          </Col>
           <Col sm={12} lg={6}>
             {this.deviceInfo}
           </Col>
